d42d7bb1eb27d125ce497ade4c2c5c8e675d5f4a
Ahmed Amged <ahmedamged@live.com>
2023-07-14 12:00:39 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char reversedAlphapet = 'z';
+
+	while (reversedAlphapet > 'a')
+	{
+		putchar(reversedAlphapet);
+		reversedAlphapet--;
+	}
+	putchar('\n');
+	return (0);
+}
