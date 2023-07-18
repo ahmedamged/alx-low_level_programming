4be17da818c65bc5f357a6940d3600b0517c717d
Ahmed Amged <ahmedamged@live.com>
2023-07-18 15:39:04 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * add - Adds two numbers
+ * @n: number
+ * @m: number
+ * Return: the result of summation
+ */
+int add(int n, int m)
+{
+        int r = n + m;
+
+        return (r);
+}
