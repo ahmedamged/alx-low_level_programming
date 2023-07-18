3a39cafe512c1a9c8f322c91eaec53874916031d
Ahmed Amged <ahmedamged@live.com>
2023-07-18 14:32:28 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * times_table - prints times table
+ * Return: void
+ */
+void times_table(void)
+{
+	int horizontal = 0;
+	int vertical = 0;
+	int result = 0;
+
+	while (horizontal < 10)
+	{
+		while (vertical < 10)
+		{
+			result = horizontal * vertical;
+			printf("%d", result);
+			if (vertical != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			vertical++;
+		}
+		putchar('\n');
+		vertical = 0;
+		horizontal++;
+	}
+}
