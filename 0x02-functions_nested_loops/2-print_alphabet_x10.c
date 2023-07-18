816e155be0e4ff558bf3bae987d8f28cb6d99fd1
Ahmed Amged <ahmedamged@live.com>
2023-07-18 10:57:40 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_alphabet_x10 - Prints Alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char alphapet = 'a';
+	int i = 0;
+
+	while (i < 9)
+	{
+		while (alphapet <= 'z')
+		{
+			putchar(alphapet);
+			alphapet++;
+		}
+		putchar('\n');
+	}
+}
