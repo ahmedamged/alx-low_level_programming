14e517738ec0255035d7e405e084bd98c5877360
Ahmed Amged <ahmedamged@live.com>
2023-07-18 10:10:58 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Description: 'The program description'
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Functions & Nestesd Loops
