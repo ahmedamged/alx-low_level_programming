2a5189a01785973c5f4e056571e0a0a12c5c805f
Ahmed Amged <ahmedamged@live.com>
2023-07-18 14:17:03 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hoursDig = 0;
+	int minutesDig = 0;
+	while (hoursDig < 24)
+	{
+		while (minutesDig < 60)
+		{
+			printf("%02d:%02d", hoursDig, minutesDig);
+			putchar('\n');
+			minutesDig++;
+		}
+		minutesDig = 0;
+		hoursDig++;
+	}
+}
