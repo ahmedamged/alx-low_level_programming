d89cca8d61ae110ca8e56128972acccc04933ddb
Ahmed Amged <ahmedamged@live.com>
2023-07-20 03:21:41 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * _isupper - Checks if charachter c is uppercase
+ * @c: charachter
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+        int r = isupper(c);
+
+        if (r == 0)
+        {
+                return (0);
+        }
+        else
+        {
+                return (1);
+        }
+}
