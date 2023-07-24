c2535ea1dd8a7c8c99cb24e86bfae811034580b6
Ahmed Amged <ahmedamged@live.com>
2023-07-24 16:05:39 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - Puts even positions of a string
+ * @str: string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (str[j] != '\0')
+	{
+		j++;
+	}
+	while (i < j)
+	{
+		if (i % 2 == 0)
+		{
+			temp = str[i];
+			printf ("%c", temp);
+		}
+		i++;
+	}
+	putchar('\n');
+}
