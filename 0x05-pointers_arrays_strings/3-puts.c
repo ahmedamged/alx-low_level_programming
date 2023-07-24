843855f0a852be2f3c003d6f5399210d4bf8290c
Ahmed Amged <ahmedamged@live.com>
2023-07-24 09:27:58 -0700

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _puts - Prints a string
+ * @str: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
