3931d7dfe8417f514caf1e48214fb31d9db671d2
Ahmed Amged <ahmedamged@live.com>
2023-07-27 05:00:29 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,12 @@
  * @s1: the source string
  * @s2: the destination string
  * Return: integer less than, equal to, or greater than zero
- * if s1 is found, respectively, 
+ * if s1 is found, respectively,
  * to be less than, to match, or be greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
 	int y = strcmp(s1, s2);
+
 	return (y);
 }
