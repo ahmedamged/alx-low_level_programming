d98eae3db3591fe85e5818fa0898db6467263b0a
Ahmed Amged <ahmedamged@live.com>
2023-08-03 06:20:47 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: array of charachters
+ * Return: void
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
