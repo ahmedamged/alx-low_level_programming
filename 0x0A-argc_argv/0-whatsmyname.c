cea4596f59b34a2f618ae7b69332f32b6f34c471
Ahmed Amged <ahmedamged@live.com>
2023-08-04 10:11:57 -0700

    It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,14 @@
 /**
  * main - Entry point
  * Description: a program that prints its name
+ * @argc: number of arguments
+ * @argv: array of strings which are arguments
  * Return: Always 0 - (success)
  */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
-	{	
+	{
 		printf("%s\n", argv[0]);
 	}
 	return (0);
