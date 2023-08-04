23f3143ccccbe193179a17c5ab93d40460278c94
Ahmed Amged <ahmedamged@live.com>
2023-08-04 01:15:36 -0700

    They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -5,35 +5,52 @@
 #include "main.h"
 
 /**
+ * palindrome - Checks if a string is a palindrome
+ * @s: array of characters
+ * @i: iterator from the beginning
+ * @j: iterator from the end
+ * Return: 1 if a string is a palindrome and 0 if not
+ */
+int palindrome(char *s, int i, int j)
+{
+	if (s[i] == s[j] && i == j)
+	{
+		return (1);
+	}
+	else if (s[i] == s[j] && i == j - 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (j > i)
+		{
+			return (palindrome(s, i + 1, j - 1));
+		}
+		else
+		{
+			return (0);
+		}
+	}
+}
+
+/**
  * is_palindrome - Checks if a string is a palindrome
  * @s: array of characters
  * Return: 1 if a string is a palindrome and 0 if not
  */
 int is_palindrome(char *s)
 {
-	int i = strlen(s) - 1;
-	int j = 0;
-	char ch;
-	char *str = malloc(strlen(s));
+	int len = strlen(s);
 
-	if (strlen(s) == 0)
-	{
-		return (1);
-	}
-	while (i > 0)
-	{
-		ch = s[i];
-		str[i] = s[j];
-		str[j] = ch;
-		i--;
-		j++;
-	}
-	if (strcmp(str, s) == 0)
+	if (len == 0)
 	{
 		return (1);
 	}
 	else
 	{
-		return (0);
+		int result = palindrome(s, 0, len - 1);
+
+		return (result);
 	}
 }
