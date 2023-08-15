fd498b0fd75628781e0db7a2f16c34203e305856
Ahmed Amged <ahmedamged@live.com>
2023-08-15 09:48:39 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,18 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * struct dog - dog characterstics
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of the owner of the dog
- * Description: Definesdog characterstics
+ * struct dog - Dog characterstics
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ * Description: Dog characterstics
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-}
+        char *name;
+        float age; 
+        char *owner;
+};
 
 #endif
