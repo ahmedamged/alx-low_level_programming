5ec89aebce4ea059d7d86a66a2bc59c38b789dcb
Ahmed Amged <ahmedamged@live.com>
2023-08-15 11:31:08 -0700

    A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,9 +20,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	d->name = cname;
+	d->name = name;
 	d->age = age;
-	d->owner = cowner;
+	d->owner = owner;
 
 	return (d);
 }
