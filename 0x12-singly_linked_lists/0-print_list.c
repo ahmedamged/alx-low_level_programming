a844be54fb40d35f497c7722ca1e6967a1855c1d
ahmedamged <ahmedamged@live.com>
2023-08-24 08:16:42 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,19 +9,18 @@
 size_t print_list(const list_t *h)
 {
 	size_t n = 0;
-	const list_t *current = h;
 
-	while (current != NULL)
+	while (h != NULL)
 	{
-		if (current->str == NULL)
+		if (h->str == NULL)
 		{
 			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s\n", current->len, current->str);
+			printf("[%d] %s\n", h->len, h->str);
 		}
-		current = current->next;
+		h = h->next;
 		n++;
 	}
 	return (n);
